e7b4cb41ad619a55ba3fe575beb4633c56989fa5
vitorotto <vitorotto05@gmail.com>
2025-04-16 22:49:44 -0300

    Código da aula 06

--- projeto_produtos/produtos/urls.py
+++ projeto_produtos/produtos/urls.py
@@ -4,4 +4,9 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('produtos/', views.produtos, name='produtos'),
+    path('produtos/preco/', views.produtos_por_preco, name='produtos_por_preco'),
+    path('produtos/parametro/<parametro>',
+         views.produtos_por_parametro, name='produtos_por_parametro'),
+    path('produtos/<id_produto>', views.produto_detalhes,
+         name='url_produto_detalhes'),
 ]
--- projeto_produtos/produtos/views.py
+++ projeto_produtos/produtos/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from .models import Produto
+from django.db.models.functions import Lower
+
 
 # Create your views here.
 
@@ -18,3 +20,39 @@ def produtos(request):
         'produtos': lista_produtos,  # Passando a lista de produtos para o dicionario
     }
     return render(request, 'produtos/produtos.html', dados_index)
+
+
+def produtos_por_preco(request):
+    # Vamos pegar todos os produtos do banco de dados
+    lista_produtos = Produto.objects.all().order_by('preco')
+    dados_index = {
+        'produtos': lista_produtos,  # Passando a lista de produtos para o dicionario
+    }
+    return render(request, 'produtos/produtos.html', dados_index)
+
+
+def produtos_por_parametro(request, parametro):
+    # Vamos filtrar os produtos com base no parâmetro e valor fornecidos
+    if parametro == 'nome':
+        lista_produtos = Produto.objects.all().order_by(Lower('nome'))
+    elif parametro == 'preco':
+        lista_produtos = Produto.objects.all().order_by('preco')
+    elif parametro == 'descricao':
+        lista_produtos = Produto.objects.all().order_by(Lower('descricao'))
+    else:
+        lista_produtos = Produto.objects.all().order_by('id')
+
+    dados_index = {
+        'frase': 'Frase para testar lá no Frontend',
+        'nome': 'Lucas',
+        'idade': 25,
+        'produtos': lista_produtos,  # Passando a lista de produtos para o dicionário
+    }
+    return render(request, 'produtos/produtos.html', dados_index)
+
+def produto_detalhes(request, id_produto):
+    produto_filtrado = Produto.objects.get(id=id_produto)
+    dados_produto_detalhes = {
+        'produto': produto_filtrado,  # Passando o produto para o dicionário
+    }
+    return render(request, 'produtos/produto_detalhes.html', dados_produto_detalhes)
